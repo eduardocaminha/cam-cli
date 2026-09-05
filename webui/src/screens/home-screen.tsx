3f91379acfcb2f22547fb92be8a42ea2b02510b2
Eduardo Caminha <85403866+eduardocaminha@users.noreply.github.com>
2026-09-05 07:34:49 -0300

    GSHIP-794: Preservar o grupo de controles na moldura alinhada (#657)
    Co-authored-by: cam-worker <cam-worker@anthropic.com>

--- webui/src/screens/home-screen.tsx
+++ webui/src/screens/home-screen.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import type { AppProps } from '../app-props.ts';
+import { INSPECTOR_COLUMN_CLASS } from '../app-shell.tsx';
 import { LOCALE_CATALOG } from '../locale.ts';
 import { ConversationColumn } from './conversation.tsx';
 import { TEXT_LINK_CLASS } from './operator-links.ts';
@@ -26,7 +27,7 @@ export function HomeSurface(props: AppProps & { inspectorOpen: boolean; projectI
 			/>
 			{props.inspectorOpen ? <aside
 				aria-label={localeCatalog.runInspector.homeAccessibleLabel}
-				className="flex w-full min-w-0 flex-col gap-6 p-4 pt-0 lg:p-6 lg:pt-0 xl:w-96 xl:shrink-0 xl:overflow-y-auto xl:pt-6"
+				className={`flex w-full min-w-0 flex-col gap-6 p-4 pt-0 lg:p-6 lg:pt-0 ${INSPECTOR_COLUMN_CLASS} xl:overflow-y-auto xl:pt-6`}
 				data-slot="run-inspector"
 			>
 				<RunCard
